17ff90f46fe5dfea0969876575a8bd0a37b48748
Michelangelo <youngbrennin20@gmail.com>
2018-08-06 15:46:55 -0700

    added some stuff

---
+++ server.js
@@ -0,0 +1,41 @@
+// all the dependencies
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+// data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+// router
+require('./app/routing/apiRoutes.js')(app); 
+require('./app/routing/htmlRoutes.js')(app);
+
+// server listening
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
